7e69bd59d4f2bb14b91077ad1226e3f97ace9881
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2013-02-25 13:56:43 -0500

    Fixed a pointer issue.  (See what happens when you miss an asterisk?)

--- Team0322CommandBasedRobot2013/Subsystems/Feeder.h
+++ Team0322CommandBasedRobot2013/Subsystems/Feeder.h
@@ -12,7 +12,7 @@ class Feeder: public Subsystem {
 private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
-	Victor discFeeder;
+	Victor *discFeeder;
 public:
 	Feeder();
 	void InitDefaultCommand();
