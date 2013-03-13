0fdcb3e4414ec1ee58121b4ab1ce1e2999be59aa
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2013-03-13 15:15:59 -0400

    Added Chassis and Shooter requirements and data.

--- Team0322CommandBasedRobot2013/Commands/SmartDashboardUpdater.cpp
+++ Team0322CommandBasedRobot2013/Commands/SmartDashboardUpdater.cpp
@@ -4,8 +4,9 @@
 SmartDashboardUpdater::SmartDashboardUpdater() {
 	// Use requires() here to declare subsystem dependencies
 	// eg. requires(chassis);
-	//requires(chassis);
+	Requires(chassis);
 	Requires(chassisSensors);
+	Requires(shooter);
 }
 
 // Called just before this Command runs the first time
@@ -15,14 +16,19 @@ void SmartDashboardUpdater::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void SmartDashboardUpdater::Execute() {
-	SmartDashboard::PutNumber("Gyro Angle",chassisSensors->getGyroAngle());
-	SmartDashboard::PutNumber("Left Drive Encoder",chassisSensors->getLeftDistance());
-	SmartDashboard::PutNumber("Right Drive Encoder",chassisSensors->getRightDistance());
-	SmartDashboard::PutNumber("X-Axis Accelerometer",chassisSensors->getAccelerometerXData());
-	SmartDashboard::PutNumber("Y-Axis Accelerometer",chassisSensors->getAccelerometerYData());
-	SmartDashboard::PutNumber("Z-Axis Accelerometer",chassisSensors->getAccelerometerZData());
-	SmartDashboard::PutNumber("Left Drive Stick",oi->getDriveStickLeft()->GetY());
-	SmartDashboard::PutNumber("Right Drive Stick",oi->getDriveStickRight()->GetY());
+	SmartDashboard::PutNumber("Gyro Angle", chassisSensors->getGyroAngle());
+	SmartDashboard::PutNumber("Left Drive Encoder", chassisSensors->getLeftDistance());
+	SmartDashboard::PutNumber("Right Drive Encoder", chassisSensors->getRightDistance());
+	SmartDashboard::PutNumber("X-Axis Accelerometer", chassisSensors->getAccelerometerXData());
+	SmartDashboard::PutNumber("Y-Axis Accelerometer", chassisSensors->getAccelerometerYData());
+	SmartDashboard::PutNumber("Z-Axis Accelerometer", chassisSensors->getAccelerometerZData());
+	SmartDashboard::PutNumber("Left Drive Stick", oi->getDriveStickLeft()->GetY());
+	SmartDashboard::PutNumber("Right Drive Stick", oi->getDriveStickRight()->GetY());
+	SmartDashboard::PutNumber("Left Front Motor", chassis->getSpeedControllerOutput(1));
+	SmartDashboard::PutNumber("Left Rear Motor", chassis->getSpeedControllerOutput(3));	
+	SmartDashboard::PutNumber("Right Front Motor", chassis->getSpeedControllerOutput(2));
+	SmartDashboard::PutNumber("Right Rear Motor", chassis->getSpeedControllerOutput(4));
+	SmartDashboard::PutBoolean("Shooter State", shooter->getShooterState());
 }
 
 // Make this return true when this Command no longer needs to run execute()
